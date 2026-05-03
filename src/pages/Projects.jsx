b8e73ca5c304a0f1e1e88a53e9f9e38df82b7268
Givi <givisab12345@gmail.com>
2026-05-03 21:41:56 +0200

    Soundrush live demo added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -43,7 +43,7 @@ const myProjects = [
     description: "Mobile Rythmus-Spiel mit eigenerstelltem Design und Animationen.",
     tech: ["Kotlin", "Libgdx", "Figma"],
     github: "https://git.thm.de/mib-mase-ws25/teama",
-    live: null,
+    live: "https://drive.google.com/file/d/13CzTQ1khmACJD4_01M8NxM_LEJc4h9aI/view?usp=sharing",
   },
   {
     id: 6,
